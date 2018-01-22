f8c1cadc23524a86875bbfac4bc42e6b5edfd633
zhikiri <zhivitsa.kirill.gm@gmail.com>
2018-01-22 17:48:40 +0200

    F-2 - added new decorator realization

--- src/Pdecorate/Pdecorate.php
+++ src/Pdecorate/Pdecorate.php
@@ -4,5 +4,61 @@ namespace Pdecorate;
 
 class Pdecorate
 {
+    private static $fns = [];
 
+    private $decorators = [];
+
+    public function __construct(...$arguments)
+    {
+        $decorating = array_pop($arguments);
+        $names = array_filter($arguments, [Pdecorate::class, 'has']);
+
+        $this->decorators = array_merge(
+            [$decorating],
+            array_map(function ($name) {
+                return self::$fns[$name];
+            }, $names)
+        );
+    }
+
+    public static function add($name, $fn)
+    {
+        if (!is_callable($fn)) {
+            throw new \InvalidArgumentException('Decorator function is not callable');
+        }
+        self::$fns[$name] = $fn;
+    }
+
+    public static function remove($name)
+    {
+        unset(self::$fns[$name]);
+    }
+
+    public static function has($name)
+    {
+        return isset(self::$fns[$name]);
+    }
+
+    public function __toString()
+    {
+        return (string)$this->call();
+    }
+
+    public function call(...$arguments)
+    {
+        return array_reduce(
+            $this->decorators,
+            function ($carry, $fn) use ($arguments) {
+                return $carry = $carry === null
+                    ? call_user_func($fn, ...$arguments)
+                    : call_user_func($fn, $carry, ...$arguments);
+            },
+            null
+        );
+    }
+
+    public function __invoke(...$arguments)
+    {
+        return $this->call(...$arguments);
+    }
 }
